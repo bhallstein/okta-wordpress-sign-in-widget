c6e1cf7f7e5f627606b4d493d64ed53430c5beea
bhallstein <a@ben.am>
2022-07-27 16:59:12 +0100

    fix postmessage issue

--- includes/initialize-widget.js.php
+++ includes/initialize-widget.js.php
@@ -1,16 +1,14 @@
 <?php
   $issuer = get_option('okta-issuer-url');
-  $loginRedirect = get_option('okta-widget-login-redirect') ?: get_home_url();
   $baseUrl =
     parse_url($issuer, PHP_URL_SCHEME) . '://' .
     parse_url($issuer, PHP_URL_HOST);
-
   $wp_url = get_site_url();
 ?>
 
 var oktaSignIn = new OktaSignIn({
   baseUrl: '<?php echo $baseUrl ?>',
-  redirectUri: '<?php echo $loginRedirect ?>',
+  redirectUri: '<?php echo $wp_url ?>',
   clientId: '<?php echo get_option('okta-widget-client-id') ?>',
   scopes: '<?php echo apply_filters('okta_widget_token_scope', 'openid email') ?>'.split(' '),
   issuer: '<?php echo get_option('okta-issuer-url') ?>',
